abbaddd7dfec250bf1c592465a4f5f2665faf3e3
Matthias <tthias@hotmail.fr>
2024-06-24 10:52:55 +0200

    création de la Média pour mobile comme demander par le l'évaluateur

--- wp-content/themes/Planty/footer.php
+++ wp-content/themes/Planty/footer.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/theme.css">
-		<title>Planty</title>
-	</head>
-<body>
 	<footer class="monFooter">
 		<p>
 			<a class="effetMonFooter" href="http://localhost/planty/">Mentions légales</a>
